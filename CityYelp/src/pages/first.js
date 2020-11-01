c26a75b660018ab2b37d8f68689995f4686eb93a
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-11-01 14:03:21 +0300

    imaged add

---
+++ CityYelp/src/pages/first.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const first = (props) => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text></Text>
+            </View>
+        </SafeAreaView>
+        
+    )
+}
+export default first;
